5d3a7b1eaa5c32deee56f0e373106cca1ed0d6a7
FR-DN-99 <62807462+FR-DN-99@users.noreply.github.com>
2021-04-03 13:01:08 +0100

    Add files via upload

---
+++ Practicas/Practica4/4/4.js
@@ -0,0 +1,5 @@
+function countElements() {
+    console.log(`Numero de divs: ${document.getElementsByTagName("DIV").length}`);
+    console.log(`Numero de enlaces: ${document.getElementsByTagName("A").length}`);
+    console.log(`Numero de parrafos: ${document.getElementsByTagName("P").length}`);
+}
